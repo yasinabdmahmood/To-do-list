56ca0a22efa3ba440cec6895679056a8010cc4b4
yasinabdmahmood <abd181103@gmail.com>
2022-08-16 17:14:08 +0300

    Use filter method for clearing the checked tasks

--- src/modules/clearCompleted.js
+++ src/modules/clearCompleted.js
@@ -1,17 +1,15 @@
 import getTasks from './tasks.js';
-import removeTaskFromLocalStorage from './removeTaskFromLoaclStorage.js';
+
 
 const clearCompleted = () => {
   const arr = getTasks();
-
-  document.querySelectorAll('.taks-wrapper').forEach((element, index) => {
-    if (arr[index].complete) {
-      const task = element.querySelector('input').value;
-      removeTaskFromLocalStorage(task);
-      element.remove();
-    }
-  });
-  arr.sort((b, a) => b.index - a.index);
-  // localStorage.setItem('data',JSON.stringify(arr))
+  const dom=document.querySelectorAll('.taks-wrapper');
+ const arr2=arr.filter((element,index)=>{
+    if(element.complete){dom[index].remove()}
+   return !element.complete
+ })
+ 
+ arr2.sort((b, a) => b.index - a.index);
+ localStorage.setItem('data',JSON.stringify(arr2))
 };
 export default clearCompleted;
