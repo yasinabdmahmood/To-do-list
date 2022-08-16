db2eb8317682704521823f0901a4101540f6c4e6
yasinabdmahmood <abd181103@gmail.com>
2022-08-16 17:16:35 +0300

    Fix linter errors

--- src/modules/clearCompleted.js
+++ src/modules/clearCompleted.js
@@ -1,15 +1,14 @@
 import getTasks from './tasks.js';
 
-
 const clearCompleted = () => {
   const arr = getTasks();
-  const dom=document.querySelectorAll('.taks-wrapper');
- const arr2=arr.filter((element,index)=>{
-    if(element.complete){dom[index].remove()}
-   return !element.complete
- })
- 
- arr2.sort((b, a) => b.index - a.index);
- localStorage.setItem('data',JSON.stringify(arr2))
+  const dom = document.querySelectorAll('.taks-wrapper');
+  const arr2 = arr.filter((element, index) => {
+    if (element.complete) { dom[index].remove(); }
+    return !element.complete;
+  });
+
+  arr2.sort((b, a) => b.index - a.index);
+  localStorage.setItem('data', JSON.stringify(arr2));
 };
 export default clearCompleted;
